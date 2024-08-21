d699fd020c371eada447e4c4da95581dca2d6747
Babina081 <babitamoo333@gmail.com>
2024-08-21 15:45:42 +0545

    add dashboard file layout

--- app/dashboard/files/[id]/page.tsx
+++ app/dashboard/files/[id]/page.tsx
@@ -1,13 +1,42 @@
+import PDFView from "@/components/PDFView";
+import Chat from "@/components/Chat";
+import { adminDb } from "@/firebaseAdmin";
+import { auth } from "@clerk/nextjs/server";
 import React from "react";
 
-const ChatToFilePage = ({
+const ChatToFilePage = async ({
   params: { id },
 }: {
   params: {
     id: string;
   };
 }) => {
-  return <div>ChatToFilePage {id}</div>;
+  auth().protect();
+  const { userId } = await auth();
+
+  const ref = await adminDb
+    .collection("users")
+    .doc(userId!)
+    .collection("files")
+    .doc(id)
+    .get();
+
+  const url = ref.data()?.downloadUrl;
+
+  return (
+    <div className="grid lg:grid-cols-5 h-full overflow-hidden">
+      {/* right */}
+      <div className="col-span-5 lg:col-span-2 overflow-y-auto">
+        {/* chat */}
+        <Chat id={id}></Chat>
+      </div>
+      {/* left */}{" "}
+      <div className="col-span-5 lg:col-span-3 bg-gray-100 border-r-2 lg:border-indigo-600 lg:-order-1 overflow-auto">
+        {/* pdfview */}
+        <PDFView url={url}></PDFView>
+      </div>
+    </div>
+  );
 };
 
 export default ChatToFilePage;
