209289594cd830f6f3b1e64b5389854ec6e57e97
Babina081 <babitamoo333@gmail.com>
2024-08-21 15:45:20 +0545

    add chat component

---
+++ components/Chat.tsx
@@ -0,0 +1,121 @@
+"use client";
+import React from "react";
+import { FormEvent, useEffect, useRef, useState, useTransition } from "react";
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+import { Loader2Icon } from "lucide-react";
+import { useUser } from "@clerk/nextjs";
+import { collection, orderBy, query } from "firebase/firestore";
+import { db } from "@/firebase";
+import { useCollection } from "react-firebase-hooks/firestore";
+import { askQuestion } from "@/actions/askQuestion";
+
+export type Message = {
+  id?: string;
+  role: "human" | "ai" | "placeholder";
+  message: string;
+  createdAt: Date;
+};
+
+const Chat = ({ id }: { id: string }) => {
+  const { user } = useUser();
+  const [input, setInput] = useState("");
+  const [isPending, startTransition] = useTransition();
+  const [messages, setMessages] = useState<Message[]>([]);
+
+  const [snapshot, loading, error] = useCollection(
+    user &&
+      query(
+        collection(db, "users", user?.id, "files", id, "chat"),
+        orderBy("createdAt", "asc")
+      )
+  );
+
+  useEffect(() => {
+    if (!snapshot) return;
+    console.log("Updated snapshot", snapshot.docs);
+    const lastMessage = messages.pop();
+    if (lastMessage?.role === "ai" && lastMessage.message === "Thinking...") {
+      return;
+    }
+    const newMessages = snapshot.docs.map((doc) => {
+      const { role, message, createdAt } = doc.data();
+      return {
+        id: doc.id,
+        role,
+        message,
+        createdAt: createdAt.toDate(),
+      };
+    });
+    setMessages(newMessages);
+  }, [snapshot]);
+
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+    const q = input;
+    setInput("");
+    setMessages((prev) => [
+      ...prev,
+      {
+        role: "human",
+        message: q,
+        createdAt: new Date(),
+      },
+      {
+        role: "ai",
+        message: "Thinking...",
+        createdAt: new Date(),
+      },
+    ]);
+    startTransition(async () => {
+      const { success, message } = await askQuestion(id, q);
+
+      if (!success) {
+        // toast...
+
+        setMessages((prev) =>
+          prev.slice(0, prev.length - 1).concat([
+            {
+              role: "ai",
+              message: `Whoops... ${message}`,
+              createdAt: new Date(),
+            },
+          ])
+        );
+      }
+    });
+  };
+  return (
+    <div className="flex flex-col h-full overflow-scroll">
+      {/* chat content */}
+      <div className="flex-1 w-full">
+        {/* chat messages... */}
+        {messages.map((message) => (
+          <div key={message.id}>
+            <p>{message.message}</p>
+          </div>
+        ))}
+      </div>
+      <form
+        action=""
+        onSubmit={handleSubmit}
+        className="flex sticky bottom-0 space-x-2 p-5 bg-indigo-600/75"
+      >
+        <Input
+          placeholder="Ask a question..."
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        ></Input>
+        <Button type="submit" disabled={!input || isPending}>
+          {isPending ? (
+            <Loader2Icon className="text-indigo-600 animate-spin" />
+          ) : (
+            "Ask"
+          )}
+        </Button>
+      </form>
+    </div>
+  );
+};
+
+export default Chat;
